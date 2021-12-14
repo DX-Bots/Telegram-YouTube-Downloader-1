7c45539854b5733feb18bb780b5ecc2247024b77
ρяє∂αтσя <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-12-15 00:22:14 +0530

    Update help.py

--- workers/help.py
+++ workers/help.py
@@ -17,14 +17,14 @@ async def help(client, message):
     # Thought of somemore features but i am lazy lul
     
     helptxt = f"""/help:[📥](https://telegra.ph/file/39812237fd7a1bfc02532.jpg)\n
-                 .˜”*°•**InChat**•°*”˜.
-Copy any Valid Youtube link and paste inside the bot and follow the prompts.
-                 .˜”*°•**InGroups**•°*”˜.
+                 .˜”*°•**In Private**•°*”˜.
+Copy any Valid Youtube link and paste inside the bot and follow the steps.
+                 .˜”*°•**In Groups**•°*”˜.
 Add me in any group then copy any valid Youtube link and paste inside the bot and follow the prompts.
 
-                .📍**𝐈𝐌𝐏𝐎𝐑𝐓𝐀𝐍𝐓**📍.
--**♥ Bigger download size,more wait time ♥**
--𝐅𝐢𝐥𝐞 𝐬𝐢𝐳𝐞 𝐦𝐨𝐫𝐞 𝐭𝐡𝐞𝐧 𝟐𝐠𝐛 𝐰𝐨𝐧'𝐭 𝐛𝐞 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐝 𝐝𝐮𝐞 𝐭𝐨 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐩𝐨𝐥𝐢𝐜𝐲."""
+                .**Keep In Mind**.
+-**Bigger download File size,more wait time **
+-And Bot Can Upload at a Limit of 1.9GB Bcz TeleGram Limitations."""
         
     
     await message.reply_text(helptxt)
